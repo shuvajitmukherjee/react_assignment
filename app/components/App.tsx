890881a72e5c342e400d379c58f0bf86cf42f96c
Surya Deb <surya.deb@learningmatekol.local>
2018-06-29 17:01:09 +0530

    reset the react app

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,54 +1,10 @@
 import * as React from 'react';
-import { ShowNames } from './ShowNames';
-import { AddName } from './AddName';
 
 export class App extends React.Component {
 
-    state: {
-        username?: string,
-        dataList?: string[]
-    };
-    props: {
-        toPass?: string
-    };
-
-    constructor(props: any) {
-        super(props);
-
-        this.state = {
-            username: 'sdeb',
-            dataList: []
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.addName = this.addName.bind(this);
-    }
-
-    handleChange(eve: any) {
-        this.setState({
-            username: eve.target.value
-        });
-    }
-
-    addName(eve: string) {
-        this.setState((state: {
-            dataList: string[]
-        }) => ({
-            dataList: state.dataList.concat(eve)
-        }));
-    }
-
     render() {
         return (
-            <div> Hi {this.state.username}
-                <br />
-                Change Name: <input className="input-txt" type="text" value={this.state.username} onChange={this.handleChange} />
-                <br />
-                {this.props.toPass}
-                <br />
-                <AddName addNew={this.addName} />
-                <ShowNames names={this.state.dataList} />
-            </div>
+            <div> Hi </div>
         );
     }
 }
